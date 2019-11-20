9c92cf9ec37b80288bcf46ce6adb4f7e56a9eb6b
Jeff Hykin <jeff.hykin@gmail.com>
2019-11-20 17:17:07 -0600

    - add installer

---
+++ install.rb
@@ -0,0 +1,48 @@
+require 'atk_toolbox'
+
+
+# if the vs code command doesn't exist
+if not Console.has_command("code")
+    # pick an operating system
+    if OS.is?('mac')
+        # overwrite all system links for python2 encase anything was broken from previous installs
+        -"brew cask install visual-studio-code"
+    elsif OS.is?('windows')
+        -"scoop install vscode"
+    elsif OS.is?('linux')
+        if OS.is?('ubuntu')
+            version = OS.version
+            should_install = false
+            if version.major == 16 || version.major == 18
+                should_install = true
+            else
+                answer = Console.ask(<<-HEREDOC.remove_indent
+                    This script is only setup for Ubuntu 18 and 16
+                    Would you like to try and use the Ubuntu 18/16 installer
+                    even though you're not on Ubuntu 18/16?
+                HEREDOC
+                if answer == true
+                    should_install = true
+                end
+            end
+            
+            if not should_install
+                raise "Visual Studio Code not installed. Please install it manually".red
+            else
+                -"sudo apt update"
+                -"sudo apt install software-properties-common apt-transport-https wget"
+                -"wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -"
+                -"sudo add-apt-repository \"deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main\""
+                -"sudo apt update"
+                -"sudo apt install code"
+            end
+        else
+            raise <<-HEREDOC.remove_indent.red
+                
+                
+                The auto installer for VS Code doesn't yet support your OS
+                Please install Visual Studio Code manually
+            HEREDOC
+        end
+    end
+end
